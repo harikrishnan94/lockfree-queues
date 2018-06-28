52c2671980191889930983913bb43e23af779204
Harikrishnan <harikrishnan.prabakaran@gmail.com>
2018-06-28 20:33:45 +0530

    move cqueue.hpp from cqueue to src

--- test/src/cqueue_test.cpp
+++ test/src/cqueue_test.cpp
@@ -1,38 +1,38 @@
 #include "catch.hpp"
 
-#include "cqueue.hpp"
+#include "cqueue.h"
 
 #define as_ptr(x) (reinterpret_cast<void *>(x))
 
 TEST_CASE("Basic Test", "[cqueue]")
 {
-	cqueue_t mpsc{ 3, 1 };
+	cqueue_mpsc_t *mpsc          = CreateMPSCQueue(3, 1);
 	std::vector<void *> pointers = { as_ptr(0x00), as_ptr(0x02), as_ptr(0x04) };
 	void *elem;
 
-	REQUIRE(mpsc.try_pop(elem) == false);
+	REQUIRE(MPSCQueueTryPop(mpsc, &elem) == false);
 
-	REQUIRE(mpsc.try_push(as_ptr(0x00)) == true);
-	REQUIRE(mpsc.try_push(as_ptr(0x02)) == true);
-	REQUIRE(mpsc.try_push(as_ptr(0x04)) == true);
+	REQUIRE(MPSCQueueTryPush(mpsc, as_ptr(0x00)) == true);
+	REQUIRE(MPSCQueueTryPush(mpsc, as_ptr(0x02)) == true);
+	REQUIRE(MPSCQueueTryPush(mpsc, as_ptr(0x04)) == true);
 
-	REQUIRE(mpsc.try_push(as_ptr(0x06)) == false);
+	REQUIRE(MPSCQueueTryPush(mpsc, as_ptr(0x06)) == false);
 
 	elem = NULL;
-	REQUIRE(mpsc.try_pop(elem) == true);
+	REQUIRE(MPSCQueueTryPop(mpsc, &elem) == true);
 	REQUIRE(elem == as_ptr(0x00));
 
-	REQUIRE(mpsc.try_push(as_ptr(0x06)) == true);
+	REQUIRE(MPSCQueueTryPush(mpsc, as_ptr(0x06)) == true);
 
 	elem = NULL;
-	REQUIRE(mpsc.try_pop(elem) == true);
+	REQUIRE(MPSCQueueTryPop(mpsc, &elem) == true);
 	REQUIRE(elem == as_ptr(0x02));
 
 	elem = NULL;
-	REQUIRE(mpsc.try_pop(elem) == true);
+	REQUIRE(MPSCQueueTryPop(mpsc, &elem) == true);
 	REQUIRE(elem == as_ptr(0x04));
 
 	elem = NULL;
-	REQUIRE(mpsc.try_pop(elem) == true);
+	REQUIRE(MPSCQueueTryPop(mpsc, &elem) == true);
 	REQUIRE(elem == as_ptr(0x06));
 }
